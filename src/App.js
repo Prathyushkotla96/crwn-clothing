251b30d4a0e6c4162e9d41cfe33fbbf4a7a209ec
Prathyushkotla96 <33094244+Prathyushkotla96@users.noreply.github.com>
2019-11-25 04:50:20 -0600

    completed signin and signup component

--- src/App.js
+++ src/App.js
@@ -32,9 +32,10 @@ class App extends React.Component {
               ...snapShot.data()
             }
           });
+          
 
         });
-        console.log(this.state);
+        
       }
 
       this.setState({currentUser:userAuth});
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -4,7 +4,7 @@ import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import {signInWithGoogle} from '../../firebase/firebase.utils';
+import {signInWithGoogle,auth} from '../../firebase/firebase.utils';
 
 class SignIn extends React.Component{
     constructor(props) {
@@ -17,17 +17,25 @@ class SignIn extends React.Component{
 
     }
 
-    handleSubmit=event =>{
+    handleSubmit= async event =>{
         event.preventDefault();
+        const {email,password}=this.state;
 
-        this.setState({email:'',password:''})
-    }
+        try {
+            await auth.signInWithEmailAndPassword(email,password);
+            this.setState({email:'',password:''});
+
+        } catch (error) {
+            console.log(error);
+            
+        }
+    };
 
     handleChange=event=>{
         const {value,name}=event.target;
 
-        this.setState({[name]:value})
-    }
+        this.setState({[name]:value});
+    };
 
     render(){
         return(
@@ -39,7 +47,7 @@ class SignIn extends React.Component{
                     <FormInput name="email" type="email" handleChange={this.handleChange} value={this.state.email} label="email" required/>
                     <FormInput name="password" 
                            type="password" 
-                           value={this.state.email}
+                           value={this.state.password}
                            handleChange={this.handleChange}
                            label="password"
                            required/>
